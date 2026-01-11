d8c0a8e3e286b4d0e72961e4390c0f234b7fa7fa
nikochiko <k+git@kaustubh.page>
2026-01-11 17:16:07 +0530

    feat: add android build

---
+++ src-tauri/gen/android/app/src/main/java/com/kaafihai/shard/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kaafihai.shard
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
